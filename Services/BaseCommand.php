a8ff4989c6588dae963a99875bac66a1193ba56d
Mr <andriy.osf@gmail.com>
2018-09-28 14:50:05 +0000

    [Update] Added command service

---
+++ Services/BaseCommand.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Commands;
+
+use Infrastructure\Models\Config;
+use Infrastructure\Models\ServiceContainerBuilderFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+abstract class BaseCommand extends Command
+{
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    /**
+     * @return ContainerBuilder
+     */
+    protected function container() : ContainerBuilder
+    {
+        if ($this->container === null) {
+            $this->container = (new ServiceContainerBuilderFactory())->create($this);
+        }
+
+        return $this->container;
+    }
+
+    /**
+     * @return Config
+     */
+    protected function config() : Config
+    {
+        return $this->container()->get('config');
+    }
+}

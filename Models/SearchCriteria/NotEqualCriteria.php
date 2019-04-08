d25b945b71ce7fa252048037093d81ee16223cd7
Serhii Bobrov <sbobrov@xlntech.co.uk>
2019-04-08 12:05:14 +0300

    [update] add new criteria

---
+++ Models/SearchCriteria/NotEqualCriteria.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Infrastructure\Models\SearchCriteria;
+
+class NotEqualCriteria implements Condition
+{
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * EqualCriteria constructor.
+     * @param string $key
+     * @param string $value
+     */
+    public function __construct(string $key, string $value)
+    {
+        $this->key = $key;
+        $this->value = $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function toCondition() : array
+    {
+        return [$this->key => $this->value];
+    }
+
+    /**
+     * @return string
+     */
+    public function type(): string
+    {
+        return SearchCriteria::WHERE_NOT_EQUAL_SIGN;
+    }
+}
